67b8ac19fdbe30dfc26799acecabb9cdbf56d156
Fernando <fernando.jesus@wba.com.br>
2022-08-04 19:04:47 -0300

    Alterações para funcionamento dos 3 módulos.

--- Fernando/fernando-operacoes/target/generated-sources/annotations/br/fernando/fernandooperacoes/mapper/AutorTransformMapperImpl.java
+++ Fernando/fernando-operacoes/target/generated-sources/annotations/br/fernando/fernandooperacoes/mapper/AutorTransformMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-04T16:35:34-0300",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_332 (Azul Systems, Inc.)"
+    date = "2022-08-04T18:16:26-0300",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
 public class AutorTransformMapperImpl extends AutorTransformMapper {
@@ -21,11 +21,11 @@ public class AutorTransformMapperImpl extends AutorTransformMapper {
 
         AutorDTO autorDTO = new AutorDTO();
 
+        autorDTO.setData_registro( autor.getData_registro() );
+        autorDTO.setDescricao( autor.getDescricao() );
+        autorDTO.setEmail( autor.getEmail() );
         autorDTO.setId( autor.getId() );
         autorDTO.setNome( autor.getNome() );
-        autorDTO.setEmail( autor.getEmail() );
-        autorDTO.setDescricao( autor.getDescricao() );
-        autorDTO.setData_registro( autor.getData_registro() );
 
         return autorDTO;
     }
@@ -38,11 +38,11 @@ public class AutorTransformMapperImpl extends AutorTransformMapper {
 
         Autor autor = new Autor();
 
+        autor.setData_registro( autorDTO.getData_registro() );
+        autor.setDescricao( autorDTO.getDescricao() );
+        autor.setEmail( autorDTO.getEmail() );
         autor.setId( autorDTO.getId() );
         autor.setNome( autorDTO.getNome() );
-        autor.setEmail( autorDTO.getEmail() );
-        autor.setDescricao( autorDTO.getDescricao() );
-        autor.setData_registro( autorDTO.getData_registro() );
 
         return autor;
     }
